6e547bad82b189e3440da8d083dece1301a26c96
AlexeiFedya <57766198+AlexeiFedya@users.noreply.github.com>
2020-05-07 22:01:42 +0300

    test Belhard

---
+++ layout-bel/src/components/app/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import  HomePage  from '../pages/home-page';
+import Loader from '../loader/loader'
+import HeaderLayout from '../header/header-layout'
+import FooterCompany from '../footer-company/footer-company'
+
+import './app.css'
+
+function App() {
+  return (
+    <main className='App background-main'>
+      <HeaderLayout/>
+      <Switch>
+        <Route
+          path='/'
+          component={HomePage}
+          exact/>
+
+        <Route
+          path='/soon'
+          component={Loader}/>    
+      </Switch>
+      <FooterCompany/>
+    </main>
+  );
+}
+
+export default App;
--- layout-bel/src/index.js
+++ layout-bel/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import ErrorBoundry from './components/error-boundry/error-boundry'
+import App from './components/app/App';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.Fragment>
+    <ErrorBoundry>
+      <Router>
+        <App />
+      </Router>
+    </ErrorBoundry>
+  </React.Fragment>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
