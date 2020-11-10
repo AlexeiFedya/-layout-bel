139bb4092bd616a637cbc537cb7c4d602bf8b7a9
AlexeiFedya <fedya5711@icloud.com>
2020-11-10 22:34:16 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/-layout-bel/layout-bel/precache-manifest.ce67fe8d8a668c29c480176a1ad85cd3.js"
+  "/-layout-bel/precache-manifest.eaab02412befd49eddc4fe3c04a7712b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/-layout-bel/layout-bel/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/-layout-bel/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
