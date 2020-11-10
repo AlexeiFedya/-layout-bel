5deaf5fcb8c861d4a17bc5c7b1f80ac2dd3564d9
AlexeiFedya <fedya5711@icloud.com>
2020-11-10 22:17:21 +0300

    add gh-pages

--- layout-bel/src/components/app/App.js
+++ layout-bel/src/components/app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect} from 'react-router-dom';
 
 import  HomePage  from '../pages/home-page';
 import Loader from '../loader/loader'
@@ -21,6 +21,7 @@ function App() {
         <Route
           path='/soon'
           component={Loader}/>    
+          <Redirect to='/' />
       </Switch>
       <FooterCompany/>
     </main>
